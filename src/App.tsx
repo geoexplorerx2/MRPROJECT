90a08d6490ec0f609cea3f5e2ebfc08f46638f7a
Farshad Nabizadeh <farshadnabizadeh1993@gmail.com>
2023-10-29 23:00:19 +0300

    updated

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import useWindowSize from './hooks/useWindowSize'
 import Desktop from './Components/Desktop'
+import SmartPhone from './Components/SmartPhone'
 const App = () => {
   const { width, height } = useWindowSize()
   return (
     <div className='w-full min-h-screen relative'>
-      {width >= 790 ? <Desktop /> : ''}
+      {width >= 790 ? <Desktop /> : <SmartPhone />}
     </div>
   )
 }
