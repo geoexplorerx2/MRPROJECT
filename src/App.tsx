98f87242d3e61de8f25dbe9bf6922452f958aca3
Farshad Nabizadeh <farshadnabizadeh1993@gmail.com>
2023-10-29 19:39:23 +0300

    CardBackground

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ const App = () => {
       </div>
       <div className='w-full h-screen absolute top-0 left-0 z-10 bg-[rgba(0,0,0,0.6)]'></div>
       <div className='w-full h-screen absolute top-0 left-0 z-20 flex justify-center items-center'>
-        <div className='w-[60%] h-[70%] bg-[rgba(255,255,255,0.9)] relative rounded-lg'>
+        <div className='w-[60%] h-[70%] bg-[rgba(255,255,255,0.9)] relative rounded-lg border-[1px] border-[rgba(236,240,241,1)]'>
           <div className='absolute top-0 left-0 z-0 w-full h-full rounded-lg opacity-[0.6]'>
             <img className='w-full h-full object-cover rounded-lg' src={CardBackground} />
           </div>
