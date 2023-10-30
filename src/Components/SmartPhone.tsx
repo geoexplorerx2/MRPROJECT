8903be349607a9df6c8656b45ba97a261d63df62
Farshad Nabizadeh <farshadnabizadeh1993@gmail.com>
2023-10-30 10:10:10 +0300

    Content Section

--- src/Components/SmartPhone.tsx
+++ src/Components/SmartPhone.tsx
@@ -15,15 +15,21 @@ const SmartPhone = () => {
                 <img className='w-full min-h-screen' src={Background} />
             </div>
             <div className='w-full min-h-screen absolute top-0 left-0 z-10 bg-[rgba(0,0,0,0.8)]'></div>
-            <div className='w-full min-h-screen absolute top-0 left-0 z-20'>
-
-            </div>
+            <div className='w-full min-h-screen absolute top-0 left-0 z-20'></div>
             <div className='absolute bottom-0 right-0 z-40 flex justify-end'>
                 <img className='w-[20%] m-1 rounded-md' src={TelegramQrCode} />
             </div>
-            <div className='w-full min-h-screen absolute top-0 left-0 z-50 flex justify-center'>
-                <div className='w-[120px] h-[120px] bg-white rounded-full translate-y-[30px]'>
+            {/* <<< Content Section >>> */}
+            <div className='w-full h-screen absolute top-0 left-0 z-50 flex justify-center overflow-y-scroll'>
+                <div className='w-full'>
+                    <div className='w-full flex justify-center'>
+                        <div className='w-[120px] h-[120px] bg-white rounded-full translate-y-[30px]'>
+                            {/* <img className='w-full h-full object-cover rounded-full' src={PersonalPhoto} /> */}
+                        </div>
+                    </div>
+                    <div className='w-full'>
 
+                    </div>
                 </div>
             </div>
         </div>
